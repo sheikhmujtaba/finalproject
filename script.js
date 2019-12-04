b222508f340b5f1bd88cae75e209e901367342d8
unknown <muji88851@gmail.com>
2019-12-04 14:29:48 -0500

    work

--- script.js
+++ script.js
@@ -12,7 +12,23 @@ var penguinPromise = d3.csv("Data.csv")
         var Arc = data.map(getArc)
         console.log(Arc)
         setup(data)
-        },
+d3.select("body")
+.append("button")
+.attr("class", "button")
+.attr("style", "vertical-align:middle")
+.append("span")
+.text("Back to home")
+.on("click", function(){
+    d3.select("svg *")
+        .remove()
+        d3.select("#xAxis")
+        .remove()
+         d3.select("#yAxis")
+        .remove()
+setup(data)
+    
+})
+},
         
         function(err)
         {
@@ -82,6 +98,7 @@ var setup = function(myarray)
     drawarray(myarray, xScale, yScale, cScale)
     drawarray1(myarray, xScale, yScale, cScale)
     drawarray2(myarray, xScale, yScale, cScale)
+    drawlegend(myarray)
 }
 
 
@@ -119,7 +136,14 @@ var drawarray1 = function(myarray, xScale, yScale, cScale){
         .datum(myarray)
     .on("click", function(){
         console.log("clicked")
-    })
+        d3.select("svg *")
+        .remove()
+        d3.select("#xAxis")
+        .remove()
+         d3.select("#yAxis")
+        .remove()
+         setup3(myarray)
+         })
         .attr("fill", "none")
         .attr("stroke", "red")
         .attr("stroke-width", 3)
@@ -147,6 +171,7 @@ var drawarray2 = function(myarray, xScale, yScale, cScale){
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 30) + "px")
                 .classed("hidden", false);
+            
         })
         .on("mouseout", function()
         {
@@ -161,7 +186,8 @@ var drawarray2 = function(myarray, xScale, yScale, cScale){
         .remove()
          d3.select("#yAxis")
         .remove()
-        
+        d3.select("#tooltip")
+                .classed("hidden", true);
         
         setup1(myarray)
         
@@ -230,7 +256,7 @@ var drawCO = function(myarray, xScale, yScale, cScale){
         .append("path")
         .datum(myarray)
         .attr("fill", "none")
-        .attr("stroke", "black")
+        .attr("stroke", "blue")
         .attr("stroke-width", 3)
     .attr("d", d3.line()
           .defined(function(d){return parseFloat(d.CO2Emissions)})
@@ -311,4 +337,112 @@ var drawGMSL = function(myarray, xScale, yScale, cScale){
     .attr("r",5)
 
 
+}
+
+var setup3 = function(myarray)
+{
+    var svg = d3.select("svg")
+    .attr("width", screen.width)
+    .attr("height", screen.height);
+    
+    svg.append("g")
+    .attr("id", "graph")
+    .attr("transform", "translate("+margins.left+ "," +margins.top+")");
+    
+    var width = screen.width - margins.left - margins.right;
+    var height = screen.height - margins.top - margins.bottom;
+    
+    var xScale = d3.scaleLinear()
+        .domain([1979, 2012])
+        .range([0, width]);
+    
+    var yScale = d3.scaleLinear()
+        .domain([3, 8])
+        .range([height, 0]);
+
+    var cScale = d3.scaleOrdinal(d3.schemeTableau10)
+    
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale)
+    
+    d3.select("svg")
+        .append("g")
+        .classed("axis", true);
+    
+    d3.select(".axis")
+        .append("g")
+        .attr("id", "xAxis")
+        .attr("transform", "translate("+margins.left+","+(margins.top+height)+")")
+        .call(xAxis);
+    
+    d3.select(".axis")
+        .append("g")
+        .attr("id", "yAxis")
+        .attr("transform","translate(30,"+margins.top+")")
+        .call(yAxis);
+    drawArctic(myarray, xScale, yScale, cScale)
+}
+var drawArctic = function(myarray, xScale, yScale, cScale){
+    d3.select("#graph")
+        .append("path")
+        .datum(myarray)
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 3)
+    .attr("d", d3.line()
+          .defined(function(d){return parseFloat(d.ArcticseaIce)})
+          .x(function(d){
+        var years = parseFloat(d.Year)
+          return xScale(years)
+    })
+        .y(function(d) {
+        var num = parseFloat(d.ArcticseaIce)
+        return yScale(num);
+    }))
+}
+
+var drawlegend = function(myarray)
+{
+    d3.select("svg")
+        .append("g")
+        .append("circle")
+        .attr("cx",150)
+        .attr("cy",20)
+        .attr("r", 7)
+        .style("fill", "red")
+     d3.select("svg")
+        .append("text")
+        .attr("x", 160)
+        .attr("y", 20)
+        .text("Change in Arctic Ice Sheets")
+        .style("font-size", "13px")
+        .attr("alignment-baseline","middle")
+    d3.select("svg")
+        .append("g")
+        .append("circle")
+        .attr("cx",340)
+        .attr("cy",20)
+        .attr("r", 7)
+        .style("fill", "blue")
+    d3.select("svg")
+        .append("text")
+        .attr("x", 350)
+        .attr("y", 20)
+        .text("Change in C02 emissions")
+        .style("font-size", "13px")
+        .attr("alignment-baseline","middle")
+     d3.select("svg")
+        .append("g")
+        .append("circle")
+        .attr("cx",500)
+        .attr("cy",20)
+        .attr("r", 7)
+        .style("fill", "black")
+    d3.select("svg")
+        .append("text")
+        .attr("x", 510)
+        .attr("y", 20)
+        .text("Change in Global Mean Sea Level")
+        .style("font-size", "13px")
+        .attr("alignment-baseline","middle")
 }
